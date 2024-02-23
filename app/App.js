20562ec5b11bb5c8ed492a1fd4f95c729bfd7133
albertog1 <alberto.gentile1@gmail.com>
2024-02-23 01:58:48 -0300

    feat: added new favorites screen

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './screen/WelcomeScreen.js';
 import BreweryDetailScreen from './screen/BreweryDetailScreen.js';
 import HomeScreen from './screen/HomeScreen.js';
+import FavoritesScreen from './screen/FavoritesScreen.js';
+
 
 const Stack = createStackNavigator();
 
@@ -13,19 +14,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="WelcomeView" component={WelcomeScreen} options={{ title: 'Beer World Wide'}}/>
+        <Stack.Screen name="WelcomeView" component={WelcomeScreen} options={{ title: 'Mr.Beer World Wide'}}/>
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home'}} />
-        <Stack.Screen name="BreweryDetail" component={BreweryDetailScreen} options={{ title: 'Detail'}} />
+        <Stack.Screen name="BreweryDetail" component={BreweryDetailScreen} options={{ title: 'Brewery Detail'}} />
+        <Stack.Screen name="Favorites" component={FavoritesScreen} options={{ title: 'Favorites'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
